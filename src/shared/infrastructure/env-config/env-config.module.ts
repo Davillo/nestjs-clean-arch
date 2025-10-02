d32f2d0cd3cb6437701f85ffe36a5189dba65c8e
Davillo Sidrim <davillo.sidrim.ext@allos.co>
2025-10-02 14:51:02 -0300

    envconfig

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return this.configService.get<number>('PORT');
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV');
+  }
+}
